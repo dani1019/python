e0bcba790d756011d8a989bfd34e14ebdb1e5053
dani1019 <nihonkita17@gmail.com>
2022-04-23 04:37:01 +0900

    modify wrong index of incomplete_word

--- game/hang_man/hangman_print.py
+++ game/hang_man/hangman_print.py
@@ -11,14 +11,17 @@ def print_word():
     #return three characters[list]
     hangman_word = 'apple'
     change_letters = random.sample(list(hangman_word),3)
+
     #one word[string]からrandomで3つのcharacterを選び'_'に変換する
     #変換された結果は、changes_word[list]に入れる
     changes_word = []
+    print(f'hangman_word: {hangman_word}')
     for letter in list(hangman_word):
         for change_letter in change_letters:
             if letter == change_letter:
                 letter = '_'
-        changes_word.append(letter) 
+        changes_word.append(letter)
+
     #changes_word[list]をstringに変換し画面に表示する
     change_word_str = ' '
     word = change_word_str.join(changes_word)
@@ -26,11 +29,13 @@ def print_word():
 
 def enter_letter(complete_word,incomplete_word,enter_letter):
     #입력받은 enter_letter을 전부 소문자로 만듦
-    print("incomplete_word" + incomplete_word)
     enter_letter_lower = enter_letter.lower()
     #제시했던 단어의 형식을 str -> list로 변환
-    complete_word_list =list(complete_word)
-    incomplete_word_list = list(incomplete_word)
+    complete_word_list =complete_word.split()
+    incomplete_word_list = incomplete_word.split()
+
+    for word in complete_word:
+        print(f'complete_word: {word}')
     #complete_word_letter에 enter_letter가 어느 index에 확인할 수 있는 리스트 작성
 
     #완성된 단어에, 입력받은 문자가 있는지 체크해서, 리스트에 넣는 처리를 만들었다.
@@ -39,34 +44,34 @@ def enter_letter(complete_word,incomplete_word,enter_letter):
     #버그 입력받은 문자가 완성된 단어가 있으면,  dict{index,enter_letter} 에 들어가게된.
     #완성된 단어 한 글자 한글자씩 for문을 돌려서 일치하는가 확인하고, 
     #일치하는 조건에서만  dict{index,enter_letter} 형태의 리스트에 넣는 것으로
-    index_dict = {}
-    for index, letter in enumerate(complete_word_list):
-            if letter == enter_letter_lower:
-                print("a")
-                print("enter_letter_lower: " + enter_letter_lower)
-                print()
-                index_dict[index] = enter_letter_lower
+    # index_dict = {}
+    # for index, letter in enumerate(complete_word_list):
+    #         if letter == enter_letter_lower:
+    #             print("a")
+    #             print("enter_letter_lower: " + enter_letter_lower)
+    #             print()
+    #             index_dict[index] = enter_letter_lower
     
-    for i,v in index_dict.items():
-        print(i,v)
-        print()
+    # for i,v in index_dict.items():
+    #     print(i,v)
+    #     print()
 
-    #incomplete_word_list 4글자인데, 8자로 출력이 되는 이유는?
-    for index,element in incomplete_word_list:
-        print(index,element)
-        print()
+    # #incomplete_word_list 4글자인데, 8자로 출력이 되는 이유는?
+    # for index,element in incomplete_word_list:
+    #     print(index,element)
+    #     print()
     
     #dic_index하고 incomplete_word_list index가 같다면 incomplete_word_list에 dic_letter을 넣기
-    for index, letter in enumerate(incomplete_word_list):
-        for dic_index, dic_letter in index_dict.items():
-            if index == dic_index:
-                incomplete_word_list[dic_index] = dic_letter
-            else:
-                print(index)
-                print()
+    # for index, letter in enumerate(incomplete_word_list):
+    #     for dic_index, dic_letter in index_dict.items():
+    #         if index == dic_index:
+    #             incomplete_word_list[dic_index] = dic_letter
+    #         else:
+    #             print(index)
+    #             print()
     
-    for index,element in enumerate(incomplete_word_list):
-        print(index , element)
+    # for index,element in enumerate(incomplete_word_list):
+    #     print(index , element)
 
                 
     #'incomplete_word_letter'
