93280a0b18731887ed40c8cc7de724548874d57a
dani1019 <nihonkita17@gmail.com>
2022-04-30 03:24:59 +0900

    caese-cipher

--- game/caeser-cipher-game/main.py
+++ game/caeser-cipher-game/main.py
@@ -1,10 +1,5 @@
 import process
 
-change_select = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-if change_select == "encode":
-    process.encode_procees()
-else:
-    print('exit to not encode')
-# for index, value in enumerate(word):
-#     print(f'{value}: {index}番名: {ord(value)}')
+change_select = input("Type 'encode' to encrypt:\n")
+process.encode_procees()
 
