f30eb9d5479fd1051ee1bce802ea450389121d27
dani1019 <nihonkita17@gmail.com>
2022-05-13 04:44:32 +0900

    refactor blackjack_game

--- blackjack_game/main.py
+++ blackjack_game/main.py
@@ -1,2 +1,3 @@
 import process_game
-process_game.select_card()
+start_game = input("Do you  want to play a game of Blackjack? Type 'y' or 'n': ")
+process_game.game_start()
--- blackjack_game/process_game.py
+++ blackjack_game/process_game.py
@@ -1,67 +1,59 @@
 import random
-sum_computer_cards = 0
-start_game = True
-my_cards = []
-computer_cards = []
-cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
-target_number = 21
+def deal_card():
+    cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+    card = random.choice(cards)
+    return card
 
-def select_card():
-    start_game = input("Do you  want to play a game of Blackjack? Type 'y' or 'n': ")
-    #カード2枚がランダムに洗濯され、私に与えられる。
-    my_cards = random.sample(cards, 2)
-    print(f'your_cards: {my_cards}')
-    # コンピューターには、ランダムに1枚のカードが与えられる
-    computer_first_card = random.choice(cards)
-    computer_cards.append(computer_first_card)
-    print(f"computer's first card: {computer_cards}")
+def game_start():
+    user_cards = []
+    computer_cards = []
+    #ユーザー、コンピューターそれぞれ2枚のカード選ぶ
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+    
+    #ユーザー、コンピューターカードを表示する
+    print(f'your card: {user_cards}')
+    print(f'computer_card: {computer_cards}')
 
-    #追加的にカードを洗濯するか選ぶ
-    add_select_card(my_cards,computer_cards)
+    #コンピューターカードを追加的に選ぶ
+    add_computer_card(computer_cards)
+    #ユーザーカードを追加的に選ぶ
+    add_user_card(user_cards,computer_cards)
 
-#y,n선택하면, 나 컴퓨터가 하나씩의 카드를 더뽑는다.
-#만약 컴퓨터의 카드값이 17이하면 강제로 하나 더 뽑게 한다.
-#만약 컴퓨터의 카드값이 17이상이면, 내 카드 값에 따라 더 뽑을지 않뽑을지 질문을 더 한다.
-
-
-def add_select_card(my_cards,computer_cards):
-    #컴퓨터 카드 뽑기
-    computer_second_card = random.choice(cards)
-    computer_cards.append(computer_second_card)
-
-    #컴퓨터 카드 합이 17이하면 하나 더 뽑기
+def add_computer_card(computer_cards):
+    #コンピューターが選んだカードの合計が17未満の場合、1枚追加する
     while sum(computer_cards) < 17:
-        print(f'sum_computer_cards2:{sum(computer_cards)}')
-        computer_second_card = random.choice(cards)
-        computer_cards.append(computer_second_card)
-        print(f'sum_computer_cards3: {sum(computer_cards)} ')
+        computer_cards.append(deal_card())
 
-    # select_y_or_n = input("Type 'y' to get another card type 'n' to pass : n: ")
-    # #yを押すと, 私にカードが1枚追加、コンピューターにカード1枚追加される    
-    # if select_y_or_n == "y":
-    #     my_second_card = random.choice(cards)
-    #     my_cards.append(my_second_card)
-    #     print(f'your card: {my_cards}')
+def add_user_card(user_cards,computer_cards):
+    target_number = 21
+    select_y_or_n = input("Type 'y' to get another card type 'n' to pass : n: ")
+    #yを押すと, 私にカードが1枚追加、コンピューターにカード1枚追加される    
+    if select_y_or_n == "y":
+        user_cards.append(deal_card())
+        print(f'your card: {user_cards}')
+        print(f'computer_card: {computer_cards}')
 
-    #     computer_second_card = random.choice(cards)
-        
-    #     print(f'computer_card: {computer_cards}')
+        add_user_card(user_cards,computer_cards)
+    #nを押すと、カードの合計を計算し、私とコンピューター中で、合計が21に近い人が勝利する
+    else:
+        print(f'your card: {user_cards}')
+        print(f'computer_card: {computer_cards}')
+        sum_user_cards = sum(user_cards)
+        sum_computer_cards = sum(computer_cards)
+        sum_user_cards_near_target_num = abs(sum_user_cards - target_number)
+        sum_computer_cards_near_target_num = abs(sum_computer_cards - target_number)
 
-    #     sum_computer_cards = sum(computer_cards)
-
-    #     add_select_card(my_cards,computer_cards)
-    # #nを押すと、カードの合計を計算し、私とコンピューター中で、合計が21に近い人が勝利する
-    # else:
-    #     sum_my_cards = sum(my_cards)
-        
-
-    #     sum_my_cards_near_target_num = abs(sum_my_cards -target_number)
-    #     sum_computer_cards_near_target_num = abs(sum_computer_cards - target_number)
-
-    #     if sum_my_cards_near_target_num < sum_computer_cards_near_target_num:
-    #         print("You win")
-    #     else:
-    #         print("You lose")
+        #ユーザー、コンピューターカードの合計が同じである場合、
+        if  sum_user_cards == sum_computer_cards:
+            print("It's draw")
+        #ユーザーカードの合計が21超えたり、ユーザーカードの合計より、コンピューターカードの合計が21に近い場合
+        elif 21 < sum_user_cards or sum_user_cards_near_target_num > sum_computer_cards_near_target_num:
+            print("You lose")
+        #ユーザーカードの合計がコンピューターカードの合計より21に近い場合
+        else:
+            print("You win")
 
 
 
