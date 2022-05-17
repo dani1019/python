dc59f7537423ff568c54190de2883eb5885a57ae
dani1019 <nihonkita17@gmail.com>
2022-05-18 05:32:45 +0900

    add make a guess method to guess_number_game

--- game/guess_number_game/start_game.py
+++ game/guess_number_game/start_game.py
@@ -1,3 +1,4 @@
+import random
 easy_attempt_num = 10
 hard_attempt_num = 5
 
@@ -8,11 +9,31 @@ def start_question():
     
     level = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
-    #予想あれる数字を入力する
-    select_level(level)
-
-def select_level(level):
     if level == "easy":
         print(f"You have {easy_attempt_num} attempts remainging to guess the number.")
     elif level == "hard":
         print(f"You have {hard_attempt_num} attempts remainging to guess the number.")
+
+    #数字をランダムに選択する
+    random_number= random_select_number()
+
+    #予想あれる数字を入力する
+    make_a_guess(random_number)
+
+def random_select_number():
+    random_number = random.randint(1, 100)
+    print(f"answer: {random_number}")
+    return random_number
+    
+    #予想あれる数字を入力する
+def make_a_guess(random_number):
+    right_check = False
+    while not right_check:
+        enter_number = int(input("Make a guess: "))
+        if enter_number < random_number:
+            print("Too Low")
+        elif enter_number > random_number:
+            print("Too high")
+        else:
+            print(f"You got it! The answer was {random_number}")
+            right_check = True
